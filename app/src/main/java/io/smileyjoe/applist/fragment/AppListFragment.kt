ad028f70e24c5ecddeb29d09b3a99a2db8a981f6
Cody Strugnell <cody.strugnell@gmail.com>
2023-10-29 17:42:12 +0200

    App list fragment

--- app/src/main/java/io/smileyjoe/applist/fragment/AppListFragment.kt
+++ app/src/main/java/io/smileyjoe/applist/fragment/AppListFragment.kt
@@ -19,31 +19,52 @@ import io.smileyjoe.applist.extensions.Compat.getSerializableCompat
 import io.smileyjoe.applist.util.Notify
 import io.smileyjoe.applist.viewholder.AppDetailViewHolder
 
+/**
+ * Fragment containing a list of apps, filtered by the [Page] details
+ */
 class AppListFragment : Fragment() {
 
+    /**
+     * Callback for when the page is loaded
+     */
     fun interface OnLoadComplete {
+        /**
+         * The page is loaded
+         *
+         * @param page the page that loaded
+         * @param appCount how many apps are in the list
+         */
         fun onLoadComplete(page: Page, appCount: Int)
     }
 
+    /**
+     * The user selected an item in the list
+     *
+     * @see [AppDetailViewHolder.OnItemSelected]
+     */
     fun interface OnItemSelected : AppDetailViewHolder.OnItemSelected
 
     companion object {
         private const val EXTRA_PAGE: String = "page"
 
-        @JvmStatic
-        fun newInstance(page: Page): AppListFragment {
-            var fragment = AppListFragment()
-            var args = Bundle()
-            args.putSerializable(EXTRA_PAGE, page)
-            fragment.arguments = args
-            return fragment
-        }
+        /**
+         * Get an instance of the fragment set to the specified [Page]
+         *
+         * @param page page details to show
+         * @return instance of the fragment
+         */
+        fun newInstance(page: Page): AppListFragment =
+            AppListFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(EXTRA_PAGE, page)
+                }
+            }
     }
 
     var isLoading: Boolean = true
     lateinit var page: Page
     lateinit var appDetailAdapter: AppDetailAdapter
-    lateinit var view: FragmentAppListBinding
+    lateinit var binding: FragmentAppListBinding
     var onLoadComplete: OnLoadComplete? = null
     var onItemSelected: OnItemSelected? = null
 
@@ -58,11 +79,11 @@ class AppListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        view = FragmentAppListBinding.inflate(layoutInflater, container, false)
+        binding = FragmentAppListBinding.inflate(layoutInflater, container, false)
 
         setupAdapter()
 
-        view.recyclerAppDetails.apply {
+        binding.recyclerAppDetails.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = appDetailAdapter
         }
@@ -70,9 +91,12 @@ class AppListFragment : Fragment() {
         populateList()
         handleDisplayView()
 
-        return view.root
+        return binding.root
     }
 
+    /**
+     * Setup the adapter
+     */
     private fun setupAdapter() {
         appDetailAdapter = AppDetailAdapter(
             page = page,
@@ -82,15 +106,26 @@ class AppListFragment : Fragment() {
         )
     }
 
+    /**
+     * Add a listener to the db, this will fire once for the first load, and then will make
+     * sure the list is kept up to date, eg, if an installed app is saved, it will immediately
+     * be added to the saved list.
+     */
     private fun populateList() {
         Db.getDetailReference(requireActivity())?.addValueEventListener(AppDetailsEventListener())
     }
 
+    /**
+     * Setup which view to display based on the information we have. Either the list will show,
+     * or the loader, or an error message.
+     */
     private fun handleDisplayView() {
+        // only one shows at a time, so set them all to GONE to start //
         var progressVisibility = View.GONE
         var recyclerVisibility = View.GONE
         var textVisibility = View.GONE
 
+        // set only the needed view to visible //
         if (isLoading) {
             progressVisibility = View.VISIBLE
         } else if (appDetailAdapter.hasApps()) {
@@ -99,13 +134,20 @@ class AppListFragment : Fragment() {
             textVisibility = View.VISIBLE
         }
 
-        view.apply {
+        // update all the visibilities //
+        binding.apply {
             progressLoading.visibility = progressVisibility
             recyclerAppDetails.visibility = recyclerVisibility
             textEmpty.visibility = textVisibility
         }
     }
 
+    /**
+     * Db listener for any updates to the firebase objects.
+     * </p>
+     * Updates the adapter on any changes and notifies the calling class
+     * that data has loaded
+     */
     inner class AppDetailsEventListener : ValueEventListener {
         override fun onDataChange(snapshot: DataSnapshot) {
             appDetailAdapter.items = page.getApps(requireContext(), snapshot)
